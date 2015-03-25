9eba0fb14014c7d8cfaa913932742a75e0f3dc12
Kaushik Dugar <kdugar24@gmail.com>
2015-03-25 14:00:09 +0530

    Initial Commit

---
+++ Tutorialtwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorialtwo
+//
+//  Created by Kaushik Dugar on 25/03/15.
+//  Copyright (c) 2015 Kaushik Dugar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
